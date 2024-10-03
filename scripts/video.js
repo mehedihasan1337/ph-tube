45c1d9b4f32cf401f08beab8f68f981b2c5a4692
mehedihasan1337 <mdmehedihasan1337@gmail.com>
2024-10-04 01:33:44 +0600

    added an js file

--- scripts/video.js
+++ scripts/video.js
@@ -1,3 +1,12 @@
+
+function getTimeString(time){
+    const hour = parseInt(time / 3600);
+    let remainingSecond = time % 3600;
+    const minute = parseInt(remainingSecond / 60);
+   remainingSecond = remainingSecond % 60;
+   return`${hour} hour ${minute} minute ${remainingSecond} second ago`;
+}
+
 // 1 - - Fetch, Load and Show Categories on html
 
 // create loadCategories
@@ -16,6 +25,14 @@ const loadvideos = () => {
         .then((data) => displayVideos(data.videos))
         .catch((error) => console.log(error));
 };
+const loadCategoryVideos =(id) => {
+    // alert(id);
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+        .then((res) => res.json())
+        .then((data) => displayVideos(data.category))
+        .catch((error) => console.log(error)); 
+
+}
 
 // const cardDemo ={
 //  category_id:"1001",
@@ -42,16 +59,24 @@ const loadvideos = () => {
 
 const displayVideos = (videos) => {
     const videoContainer = document.getElementById("videos");
+    videoContainer.innerHTML = "",
     videos.forEach((video) => {
         console.log(video);
         const card = document.createElement("div");
         card.classList = "card card-compact"
         card.innerHTML = `
-          <figure class="h-[200px]">
+          <figure class="h-[200px] relative"> 
        <img
       src=${video.thumbnail}
       class="h-full w-full object-cover"
       alt="Shoes" />
+
+      ${video.others.posted_date?.length == 0
+        ? "" :`  <span class="absolute text-xs right-2 bottom-2 bg-black text-white rounded p-1">${getTimeString(video.others.posted_date)} </span>
+        `
+      }
+
+
       </figure>
     <div class="px-0 py-2 flex gap-2">
     <div> 
@@ -61,7 +86,9 @@ const displayVideos = (videos) => {
        <h2 class="font-bold">${video.title} </h2>
        <div class="flex items-center gap-2">
         <p class="text-gray-400">${video.authors[0].profile_name}</p>
-        <img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png">
+            ${video.authors[0].verified == true ? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png"/>` 
+                : ""
+    }
         </div>
        <p></p>
     
@@ -82,11 +109,16 @@ const DisplayCategories = (categories) => {
     categories.forEach((item) => {
         console.log(item);
         // create a button
-        const button = document.createElement("button");
-        button.classList = "btn";
-        button.innerText = item.category;
+        const buttonContainer = document.createElement("div");
+        buttonContainer.innerHTML=`
+        <button onclick="loadCategoryVideos (${item.category_id})" class="btn">
+          ${item.category}
+        </button>
+        `;
+        
+        
         // add button to catagory container
-        categoryContainer.append(button);
+        categoryContainer.append(buttonContainer);
 
     });
 
---
+++ test.js
@@ -0,0 +1,35 @@
+//  const isVerified = " ";
+//  if (isVerified == true) {
+//     console.log("uset is verified");
+
+//  }
+//  else {
+//     console.log("user is not verified");
+//  }
+
+
+
+
+
+
+
+
+// Time
+
+// function getTimeString(time){
+//     const hour = parseInt(time / 3600);
+//     let remainingSecond = time % 3600;
+//     const minute = parseInt(remainingSecond / 60);
+//    remainingSecond = remainingSecond % 60;
+//    return`${hour} hour ${minute} minute ${remainingSecond} second ago`;
+
+// }
+// console.log(getTimeString(7865));
+
+
+
+
+
+
+
+
